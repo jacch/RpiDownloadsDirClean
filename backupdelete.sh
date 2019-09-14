ff47672e3f2c631ee8471bb18ba6a5e20043dcbf
陳文傑 <jacch.tw@gmail.com>
2019-09-15 01:20:05 +0800

    Update backupdelete.sh

--- backupdelete.sh
+++ backupdelete.sh
@@ -5,8 +5,13 @@ mkdir -p /home/pi/Downloads_backup/$D
 rsync -avlS /home/pi/Downloads/* /home/pi/Downloads_backup/$D
 rm -rf  /home/pi/Downloads/*
 
+rsync -avlS /home/pi/下載/* /home/pi/Downloads_backup/$D
+rm -rf  /home/pi/下載/*
+
 
 
 mkdir -p /home/pi/Pictures_backup/$D
 rsync -avlS /home/pi/Pictures/* /home/pi/Pictures_backup/$D
 rm -rf  /home/pi/Pictures/*
+rsync -avlS /home/pi/圖片/* /home/pi/Pictures_backup/$D
+rm -rf  /home/pi/圖片/*
